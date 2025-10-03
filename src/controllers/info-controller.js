49fcf692e786a75adcec5f069fa979fd573a794d
Tanuj Mishra <tanujtmmishra@gmail.com>
2025-10-03 17:26:55 +0530

    .

--- src/controllers/info-controller.js
+++ src/controllers/info-controller.js
@@ -1,7 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 
 const info = (req, res) => {
-  console.log("info-controller pe h");
   return res.status(StatusCodes.ACCEPTED).json({
     success: true,
     message: "API is live",
