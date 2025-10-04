1e8996042497060b9086f3a343641a787dfe54d2
Tanuj Mishra <93331390+Tanuj-Mishra@users.noreply.github.com>
2025-10-04 13:11:43 +0530

    Update index.js
    added json and urlencoding middlewares.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ app.get("/", (req, res) => {
   res.send("msg sent");
 });
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, (req, res) => {
